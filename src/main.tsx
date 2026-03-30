0efa2ef2b11b72f0d6efe901793d80d511fecc9e
RedJacKing <ck@idworkstudio.com>
2026-03-30 19:16:21 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,18 @@ import App from './App.tsx';
 import './index.css';
 import './i18n';
 
-// Google Analytics
+// Google Analytics - wait for script to load before firing
 window.dataLayer = window.dataLayer || [];
 function gtag(...args: any[]){ window.dataLayer.push(args); }
 (window as any).gtag = gtag;
-gtag('js', new Date());
-gtag('config', 'G-S82B3CLFH8', { 'send_page_view': true });
 
 const gtagScript = document.createElement('script');
 gtagScript.async = true;
 gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-S82B3CLFH8';
+gtagScript.onload = () => {
+  gtag('js', new Date());
+  gtag('config', 'G-S82B3CLFH8');
+};
 document.head.appendChild(gtagScript);
 
 createRoot(document.getElementById('root')!).render(
