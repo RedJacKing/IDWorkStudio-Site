4ae749532962fa44d951765f5d14fc8454e7ce38
RedJacKing <ck@idworkstudio.com>
2026-03-30 18:48:13 +0800

    main.tsx edit

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,21 @@ import App from './App.tsx';
 import './index.css';
 import './i18n';
 
+// Google Analytics
+const script1 = document.createElement('script');
+script1.async = true;
+script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-S82B3CLFH8';
+document.head.appendChild(script1);
+
+const script2 = document.createElement('script');
+script2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-S82B3CLFH8');
+`;
+document.head.appendChild(script2);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
