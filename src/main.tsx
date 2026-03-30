a05e672b6bd55e5859fc4b66b030a5adadddf7fc
RedJacKing <ck@idworkstudio.com>
2026-03-30 19:22:32 +0800

    update main and package

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,10 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './i18n';
+import ReactGA from 'react-ga4';
 
-// Google Analytics - wait for script to load before firing
-window.dataLayer = window.dataLayer || [];
-function gtag(...args: any[]){ window.dataLayer.push(args); }
-(window as any).gtag = gtag;
-
-const gtagScript = document.createElement('script');
-gtagScript.async = true;
-gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-S82B3CLFH8';
-gtagScript.onload = () => {
-  gtag('js', new Date());
-  gtag('config', 'G-S82B3CLFH8');
-};
-document.head.appendChild(gtagScript);
+ReactGA.initialize('G-S82B3CLFH8');
+ReactGA.send('pageview');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
